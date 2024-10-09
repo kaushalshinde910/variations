a70700aed5d1f88cc7bd01f6c60b276e1fd2a9bf
kaushal shinde <147977233+kaushalshinde910@users.noreply.github.com>
2024-10-09 12:30:11 +0530

    Add files via upload

---
+++ Cube_root_num.java
@@ -0,0 +1,12 @@
+package VAR1;
+//creating a cube root of a number
+public class Cube_root_num {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int a=27;
+        double b=(Math.cbrt(a));
+        System.out.println((int)b);
+	}
+
+}
